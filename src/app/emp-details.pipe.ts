3feac27e9076a8490c0976346ccf89eb39269409
Avinash405 <esarajula.avinash@gmail.com>
2019-05-08 11:23:22 +0530

    2 files modified

--- src/app/emp-details.pipe.ts
+++ src/app/emp-details.pipe.ts
@@ -5,16 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EmpDetailsPipe implements PipeTransform {
 
-  transform(param: Object[]): Object[] {
+  transform(param: string): string {
+    param = "Avinash E";
+    //let empdetailitem:Object[];
+    //for (let i; i < param.length; i++) {
+    //if (param[i]['Age'] > 18) {
+    // empdetailitem.push(param[i]['EmpName'],param[i]['Age'])
+    //}
 
-    let empdetailitem= [];
-    for (let i; i < param.length; i++) {
-      if (param[i]['Age'] > 18) {
-        empdetailitem.push(param[i]['Age'])
-      }
-
-    }
-return empdetailitem;
+    //}
+    return param;
 
   }
 
